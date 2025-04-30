139f7788922b668e0848734501ce2ef55de3b442
youngroma <krivichroma228@gmail.com>
2025-04-30 17:43:11 +0200

    Added views for displaying past launches, favorites, and launch details

--- launch_tracker/views.py
+++ launch_tracker/views.py
@@ -1,15 +1,56 @@
-from allauth.socialaccount.providers.google.views import GoogleOAuth2Adapter
-from dj_rest_auth.registration.views import SocialLoginView
-from django.contrib.sites import requests
+from datetime import datetime
+
 import requests
-from rest_framework import viewsets
-from rest_framework.response import Response
+from django.shortcuts import render
+from django.utils.timezone import make_aware
+from django.views import View
+from django.views.generic import TemplateView
+from .models import FavoriteLaunch, User
 
 
-class LaunchViewSet(viewsets.ViewSet):
-    def list(self, request):
-        response = requests.get("https://api.spacexdata.com/v4/launches")
-        return Response(response.json())
+class LaunchListView(TemplateView):
+    template_name = "launch_tracker/index.html"
 
-class GoogleLogin(SocialLoginView):
-    adapter_class = GoogleOAuth2Adapter
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        response = requests.get('https://api.spacexdata.com/v4/launches/past')
+        launches = response.json()
+
+        context['launches'] = [
+            {
+                'id': launch['id'],
+                'name': launch['name'],
+                'date': datetime.fromisoformat(launch['date_utc'].replace('Z', '+00:00')),
+                'flight_number': launch['flight_number'],
+                'details': launch.get('details'),
+                'youtube': f"https://youtu.be/{launch['links']['youtube_id']}" if launch['links'].get(
+                    'youtube_id') else None,
+                'article': launch['links'].get('article'),
+                'wikipedia': launch['links'].get('wikipedia'),
+                'image': launch['links']['patch']['small'],
+            }
+            for launch in launches
+        ]
+        return context
+
+
+class FavoritesView(TemplateView):
+    template_name = "launch_tracker/favorites.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        if self.request.user.is_authenticated:
+            context["favorites"] = FavoriteLaunch.objects.filter(user=self.request.user)
+        else:
+            context["favorites"] = []
+        return context
+
+
+class LoginView(TemplateView):
+    template_name = "launch_tracker/login.html"
+
+class LaunchDetailView(View):
+    def get(self, request, pk):
+        response = requests.get(f"https://api.spacexdata.com/v4/launches/{pk}")
+        launch = response.json()
+        return render(request, "launch_tracker/page.html", {"launch": launch})
